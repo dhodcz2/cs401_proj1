7e94c5374f11f8acc96b01b9cb2c54d1edc45302
Daniel Hodczak <dhodcz2@uic.edu>
2021-03-06 13:56:01 -0600

    setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,91 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+bool Greedy = true;
+using std::vector;
+using std::unordered_map;
+
+/**
+ Memoized implementation of standard DP algorithm for subset sum.
+ Memoization table (named _feasible):
+ 	The table is a vector of unordered maps:
+ 	vector<unordered_map<int, bool>> _feasible;
+ Interpretation:
+ 	_feasible[i][x] :
+ 		true if target x sum can be formed from a subset of a0,a1....ai
+ 		<no-entry>: recursive exploration did not explore/ned answer to sub-problem
+ */
+
+class Ssum {
+  vector<unordered_map<int, bool>> &f;
+ public:
+  unsigned long num_table_entries() {
+	unsigned int total = 0;
+	for (int i = 0; i < this->f.size(); i++) {
+	  total += f[i].size();
+	}
+	return total;
+  }
+  bool feasible(vector<unsigned int> &a, int n, unsigned int tgt) {
+	if ((n == 0 and tgt > 0) or tgt < 0)
+	  return false;
+	if (tgt == 0)
+	  return true;
+	if (f[n - 1].count(tgt) == 1)
+	  return f[n - 1][tgt];
+	else {
+	  if (Greedy)
+		f[n - 1][tgt] = feasible(a, n - 1, tgt - a[n - 1]) or feasible(a, n - 1, tgt);
+	  else
+		f[n - 1][tgt] = feasible(a, n - 1, tgt) or feasible(a, n - 1, tgt - a[n - 1]);
+	}
+	return f[n - 1][tgt];
+  }
+  explicit Ssum(vector<unordered_map<int, bool>> &f) : f(f) {}
+
+};
+
+int main(int argc, char *argv[]) {
+  int max_n, n;
+  unsigned int target, *a, val;
+  char buf[11];
+  int success;
+  vector<unsigned int> numbers;
+  vector<unordered_map<int, bool>> f;
+  unordered_map<int, bool> dummy;
+
+  if (argc < 2) {
+	fprintf(stderr, "usage: /.ssum_memo <target> [-z]\n");
+	return 0;
+  } else if (sscanf(argv[1], "%u", &target) != 1) {
+	fprintf(stderr, "bad argument '%s',\n", argv[2]);
+	return 0;
+  } else if (argc > 2) {
+	Greedy = false;
+  }
+
+  n = 0;
+  while (scanf("%u", &val) == 1) {
+	scanf("%10s", buf);
+	numbers.push_back(val);
+	f.push_back(dummy);
+	n++;
+  }
+  Ssum ssum(f);
+  printf("---- successfully read problem instance ----\n");
+  printf("        n:      %i                          \n", n);
+  printf("----    target: %i                          \n", target);
+  printf("--------------------------------------------\n");
+
+  if (ssum.feasible(numbers, n, target)) {
+	std::cout << "Number of distinct solutions:\t\t\t";
+	std::cout << "Size of smallest satisfying subset:\t\t";
+	std::cout << "Number of min-size satisfying subsets:\t";
+	std::cout << "Lexicographically first min-sized solution:";
+  } else
+	std::cout << "INFEASIBLE;";
 }
