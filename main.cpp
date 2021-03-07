731f43763c282a838bcc2d4ed9ae2584e595c96d
Daniel Hodczak <dhodcz2@uic.edu>
2021-03-06 18:57:39 -0600

    ready to rumble

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 #include <iostream>
 #include <string>
 
-bool Greedy = true;
+bool Greedy = false;
+using namespace std;
 using std::vector;
 using std::unordered_map;
 
@@ -22,30 +23,26 @@ using std::unordered_map;
  */
 
 class Ssum {
-  vector<unordered_map<int, bool>> &f;
-  vector<unsigned int> &a;
+  unordered_map<int, pair<int, string>> dict;
   const unsigned int N;
+  vector<unsigned int> a;
+  vector<unordered_map<int, bool>> f;
+  unordered_map<int, bool> dummy;
  public:
-//  Ssum(vector<unordered_map<int, bool>> &f, unsigned int N) : f(f), N(N) {}
-  Ssum(vector<unordered_map<int, bool>> &f, vector<unsigned int> &a, unsigned int N)
-	  : f(f), a(a), N(N) {};
+  Ssum(vector<pair<int, string>> &parsed) : N(parsed.size()) {
+	int n = 0;
+	for (auto &p: parsed) {
+	  a.push_back(p.first);
+	  f.push_back(dummy);
+	  dict[n++] = p;
+	}
+  }
   unsigned long num_table_entries() {
 	unsigned int total = 0;
 	for (int i = 0; i < this->f.size(); i++) {
 	  total += f[i].size();
 	}
 	return total;
-  }
-  void print() {
-	std::string column_header = "\t";
-	for (auto &y: f) {
-	  std::string s;
-	  std::vector<bool> values;
-//	  for (auto &x: y)
-//		values.push_back(x);
-//	  for (auto &x:)
-
-	}
   }
   bool feasible(int n, unsigned int tgt) {
 	if ((n == 0 and tgt > 0) or tgt < 0)
@@ -56,66 +53,47 @@ class Ssum {
 	  return f[n - 1][tgt];
 	else {
 	  if (Greedy)
-		f[n - 1][tgt] = feasible( n - 1, tgt - a[n - 1]) or feasible( n - 1, tgt);
+		f[n - 1][tgt] = feasible(n - 1, tgt - a[n - 1]) or feasible(n - 1, tgt);
 	  else
-		f[n - 1][tgt] = feasible(n - 1, tgt) or feasible( n - 1, tgt - a[n - 1]);
+		f[n - 1][tgt] = feasible(n - 1, tgt) or feasible(n - 1, tgt - a[n - 1]);
 	}
 	return f[n - 1][tgt];
   }
   int num_solutions() {
 	// TODO
+	return 0;
   }
   int smallest_subset() {
 	// TODO
+	return 0;
   }
   int num_smallest_subsets() {
 	// TODO
+	return 0;
   }
   int lex_first_solution() {
 	// TODO
+	return 0;
   }
 
 };
 
 int main(int argc, char *argv[]) {
-  unsigned int target, *a, val;
-  char buf[11];
-  int success;
-  vector<unsigned int> numbers;
-  // Dimensions S[1..N][0..T]
-  vector<unordered_map<int, bool>> f;
-  unordered_map<int, bool> dummy;
-
-  if (argc < 2) {
-	fprintf(stderr, "usage: /.ssum_memo <target> [-z]\n");
-	return 0;
-  } else if (sscanf(argv[1], "%u", &target) != 1) {
-	fprintf(stderr, "bad argument '%s',\n", argv[2]);
-	return 0;
-  } else if (argc > 2) {
-	Greedy = false;
-  }
-
-  int n = 0;
-  while (scanf("%u", &val) == 1) {
-	scanf("%10s", buf);
-	numbers.push_back(val);
-	f.push_back(dummy);
-	n++;
-  }
-  Ssum ssum(f, numbers, n);
-
-  printf("---- successfully read problem instance ----\n");
-  printf("        n:      %i                          \n", n);
-  printf("----    target: %i                          \n", target);
-  printf("--------------------------------------------\n");
-
-//  if (ssum.feasible(numbers, n, target)) {
-  if (ssum.feasible( n, target)) {
-	std::cout << "Number of distinct solutions:\t\t\t" << ssum.num_solutions();
-	std::cout << "Size of smallest satisfying subset:\t\t" << ssum.smallest_subset();
-	std::cout << "Number of min-size satisfying subsets:\t" << ssum.num_smallest_subsets();
-	std::cout << "Lexicographically first min-sized solution:" << ssum.lex_first_solution();
-  } else
+  unsigned int target = stoi(argv[1]);
+//  unordered_map<int, pair<int, string>> parsed;
+  vector<pair<int, string>> parsed;
+  int num;
+  string str;
+  while (cin >> num && cin >> str)
+	parsed.emplace_back(num, str);
+  Ssum ssum(parsed);
+  if (ssum.feasible(parsed.size(), target)) {
+	std::cout << "Number of distinct solutions:\t\t\t\t" << ssum.num_solutions() << endl;
+	std::cout << "Size of smallest satisfying subset:\t\t\t" << ssum.smallest_subset() << endl;
+	std::cout << "Number of min-size satisfying subsets:\t\t" << ssum.num_smallest_subsets() << endl;
+	std::cout << "Lexicographically first min-sized solution:\t" << ssum.lex_first_solution() << endl;
+  } else {
 	std::cout << "INFEASIBLE";
+  }
 }
+
